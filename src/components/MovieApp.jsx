32fb38e26db2add3be8ef5704302c22aec5edcd9
Ram Sanjay <ramsanjaydev08@gmail.com>
2023-11-07 23:20:44 +0530

    updated

--- src/components/MovieApp.jsx
+++ src/components/MovieApp.jsx
@@ -1,15 +1,19 @@
 import React , { useEffect, useState } from 'react'
 import axios from 'axios'
-import MovieStyles from '../CSS/Movieapp.module.css'
+import MovieContainer from './MovieContainer'
 
-function MovieApp() {
-  let [users,setusers] = useState("")
-  let [display,setDisplay] = useState([])
-  let [empField,setEmpField] = useState(false)
-  let [pageNumber,setPageNumber] = useState(1)
-  let [totalPages,setTotalPages] = useState(1)
-  let [isloading,setIsLoading] = useState(false)
+function MovieApp(props) {
 
+  //^ States
+  const [users,setusers] = useState("")
+  const [display,setDisplay] = useState([])
+  const [empField,setEmpField] = useState(false)
+  const [pageNumber,setPageNumber] = useState(1)
+  const [totalPages,setTotalPages] = useState(1)
+  const [isloading,setIsLoading] = useState(true)
+  const [isError,setIsError] = useState(false)
+
+  //^ useEffect
   useEffect(()=> {
     getMovies(pageNumber)
   },[pageNumber])
@@ -20,10 +24,17 @@ function MovieApp() {
 
   //^ Discover Api
   async function getMovies() {
+    try {
     const response = await axios.get(discover)
     setDisplay(response.data.results)
     setTotalPages(response.data.total_Pages)
-    console.log(response.data.results);
+    console.log(response.data.results)
+    setIsLoading(false)
+    setIsError(false)
+    } catch (error) {
+      setIsError(true)
+      setIsLoading(false)
+    }
   }
 
   //^ Input Api
@@ -56,42 +67,28 @@ function MovieApp() {
     defaultImage: "https://st3.depositphotos.com/32990740/35467/v/450/depositphotos_354674378-stock-illustration-isolated-picture-flat-style-icon.jpg"
   }
 
+  //^ Page Handling
   const handlePageChange = (newPage) => {
     setPageNumber(newPage)
   }
 
   return (
-    <section>
-      <div className={MovieStyles.inputContainer}>
-        <form onSubmit={onSearch} name='movieApp'>
-          <input type="text" placeholder={empField || "Search Movie"} onChange={getInputData} value={users}/>
-        </form>
-          <button onClick={searchInput}>Search</button>
-      </div>
-    <div className={MovieStyles.cardContainer}>
-      {display.map(({id,poster_path,title,vote_average,overview}) => {
-    
-        return <div key={id} className={MovieStyles.cards}>
-        <div>
-          <img src={poster_path ? imageURL + poster_path : defaultValues.defaultImage} alt={title} />
-        </div>
-          <div className={MovieStyles.cardContent}>
-          <h2>{title}</h2>
-          <h4>{vote_average ? (vote_average.toString()).slice(0, 3) : defaultValues.defaultRating}</h4>
-          </div>
-        </div>
-      })}
-    </div>
-    <div className={MovieStyles.pagination}>
-      {pageNumber > 1 && (<button onClick={() => handlePageChange(pageNumber - 1)}>Prev</button>)}
-      {pageNumber < totalPages || (<button onClick={() => handlePageChange(pageNumber + 1)}>Next</button>)}
-    </div>
-    <footer>
-      <p>IMDB Movie Rating App</p>
-    </footer>
-    </section>
+    <MovieContainer
+    display={display}
+    onSearch={onSearch}
+    getInputData={getInputData}
+    users={users}
+    searchInput={searchInput}
+    empField={empField}
+    defaultValues={defaultValues}
+    imageURL={imageURL}
+    pageNumber={pageNumber}
+    totalPages={totalPages}
+    handlePageChange={handlePageChange}
+    isloading={isloading}
+    isError={isError}
+  />
   )
 }
-
 
 export default MovieApp
---
+++ src/components/MovieContainer.jsx
@@ -0,0 +1,64 @@
+import React from 'react'
+import MovieStyles from '../CSS/Movieapp.module.css'
+
+function MovieContainer({
+  display,
+  onSearch,
+  getInputData,
+  users,
+  empField,
+  defaultValues,
+  imageURL,
+  pageNumber,
+  isLoading,
+  isError,
+  totalPages,
+  searchInput,
+  handlePageChange,
+}) {
+
+  const getRatingColorClass = (rating) => {
+    if(rating >= 7) {
+      return MovieStyles.greenColor
+    } else {
+      return MovieStyles.redColor
+    }
+  }
+
+  return (
+    <section>
+      <h1>{isLoading && "Loading"}</h1>
+      <h1>{isError && "error"}</h1>
+    <div className={MovieStyles.inputContainer}>
+      <form onSubmit={onSearch} name='movieApp'>
+        <input type="text" placeholder={empField || "Search Movie"} onChange={getInputData} value={users}/>
+      </form>
+        <button onClick={searchInput}>Search</button>
+    </div>
+  <div className={MovieStyles.cardContainer}>
+    {display.map(({id,poster_path,title,vote_average,overview}) => {
+      const ratingColorClass = getRatingColorClass(vote_average)
+      return <div key={id} className={MovieStyles.cards}>
+      <div>
+        <img src={poster_path ? imageURL + poster_path : defaultValues.defaultImage} alt={title} />
+      </div>
+        <div className={MovieStyles.cardContent}>
+        <h2>{title}</h2>
+        <h4 className={ratingColorClass}>{vote_average ? (vote_average.toString()).slice(0, 3) : defaultValues.defaultRating}<i class="fa-solid fa-star"></i></h4>
+        </div>
+      </div>
+    })}
+  </div>
+  <div className={MovieStyles.pagination}>
+    {pageNumber > 1 && (<button onClick={() => handlePageChange(pageNumber - 1)}>Prev</button>)}
+    {pageNumber < totalPages || (<button onClick={() => handlePageChange(pageNumber + 1)}>Next</button>)}
+  </div>
+  <footer>
+    <p>IMDB Movie Rating App</p>
+  </footer>
+  </section>
+  )
+}
+
+export default MovieContainer
+
